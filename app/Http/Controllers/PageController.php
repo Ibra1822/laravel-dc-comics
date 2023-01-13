80adc2545bb05cbccbb1acab3d41cc097a143ea5
“Ibra1822” <ibrahimmujagic18@gmail.com>
2023-01-13 15:17:57 +0100

    add update

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -76,9 +76,13 @@ class PageController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, Comic $comic)
     {
-        //
+        $data = $request->all();
+
+        $comic->update($data);
+
+        return redirect()->route('comics.show',$comic);
     }
 
     /**
--- resources/views/comics/edit.blade.php
+++ resources/views/comics/edit.blade.php
@@ -1,35 +1,36 @@
 @extends('layout.main')
 
 @section('content')
-<h1>Aggiungi un Nuovo Fumetto</h1>
+<h1>Modifica fumetto</h1>
 <div class="home-btn text-center">
     <a class="btn btn-primary" href="{{route('comics.index')}}">Home</a>
 </div>
-<form action="{{route('comics.store')}}" class="p-5" method="POST">
+<form action="{{route('comics.update',$comic)}}" class="p-5" method="POST">
 @csrf
+@method('PUT')
     <div class="mb-3 ">
         <label  class="form-label">Title</label>
-        <input type="text" name="title" class="form-control" >
+        <input type="text" value="{{$comic->title}}" name="title" class="form-control" >
       </div>
       <div class="mb-3">
         <label  class="form-label">Thumb</label>
-        <input type="text" name="thumb" class="form-control">
+        <input type="text" value="{{$comic->thumb}}" name="thumb" class="form-control">
       </div>
       <div class="mb-3">
         <label  class="form-label">Price</label>
-        <input type="text" name="price" class="form-control">
+        <input type="text" value="{{$comic->price}}" name="price" class="form-control">
       </div>
       <div class="mb-3">
         <label  class="form-label">Series</label>
-        <input type="text" name="series" class="form-control">
+        <input type="text" value="{{$comic->series}}" name="series" class="form-control">
       </div>
       <div class="mb-3">
         <label  class="form-label">Sale Date</label>
-        <input type="text" name="sale_date" class="form-control">
+        <input type="text" value="{{$comic->sale_date}}" name="sale_date" class="form-control">
       </div>
       <div class="mb-3">
         <label  class="form-label">Type</label>
-        <input type="text" name="type" class="form-control">
+        <input type="text" value="{{$comic->type}}" name="type" class="form-control">
       </div>
       <div class="mb-3">
         <label  class="form-label">Description</label>
